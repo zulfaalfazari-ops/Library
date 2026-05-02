7ee89ae2b572ced55ffbc13de297d6d8f54c9ff3
alaa-7bit <s145905@student.squ.edu.om>
2026-05-02 14:50:16 +0400

    edit

--- check_db.php
+++ check_db.php
@@ -1,12 +1,12 @@
 <?php
-$conn = mysqli_connect("localhost:3307", "root", "");
+echo "Start<br>";
+$conn = mysqli_connect("localhost", "root", "");
 if (!$conn) {
-    die("❌ MySQL not connected - " . mysqli_connect_error());
+    die("❌ MySQL no connection: " . mysqli_connect_error());
 }
-echo "✅ MySQL connected on port 3307<br>";
+echo "✅ Connected to MySQL<br>";
 
-$sql = "SHOW DATABASES";
-$res = mysqli_query($conn, $sql);
+$res = mysqli_query($conn, "SHOW DATABASES");
 while ($row = mysqli_fetch_assoc($res)) {
     echo $row['Database'] . "<br>";
 }
